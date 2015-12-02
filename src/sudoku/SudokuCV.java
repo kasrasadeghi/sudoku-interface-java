0af6f82904dec817031209dff3f018aa6277ec38
kasrasadeghi <kasra.sadeghi1@gmail.com>
2015-12-02 02:43:15 -0600

    0.1

---
+++ src/sudoku/SudokuCV.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sudoku;
+
+import apcscvm.DefaultControl;
+import apcscvm.View;
+import java.awt.Graphics;
+
+/**
+ *
+ * @author kasra
+ */
+public class SudokuCV extends DefaultControl<SudokuModel> implements View<SudokuModel>{
+
+    @Override
+    public void paint(SudokuModel m, Graphics grphcs, int i, int i1) {
+    }
+    
+}
